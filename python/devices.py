b3cba627b55edfadf91d58cb72c4a6bb88a07ed6
ibuibu <ibuibu69@gmail.com>
2024-12-10 14:32:32 +0900

    Add devices and metrics python code

---
+++ python/devices.py
@@ -0,0 +1,13 @@
+from key import API_CLIENT_ID, SECRET
+from lib import generate_jwt_and_send_request
+
+GRAPHQL_QUERY = """
+query {
+    devices(
+        projectIds: [ "PLEASE ENTER YOUR PROJECT IDS HERE" ],
+        after: {}
+    ) { edges { node { id } } }
+}
+"""
+
+generate_jwt_and_send_request(API_CLIENT_ID, SECRET, GRAPHQL_QUERY)
